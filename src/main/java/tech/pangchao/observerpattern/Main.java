4d7a36df9d682df5cfd5c84d24e798a5c2f80c90
pangchao <bjpangchao620@163.com>
2017-03-28 13:08:03 +0800

    add observer pattern

---
+++ src/main/java/tech/pangchao/observerpattern/Main.java
@@ -0,0 +1,23 @@
+package tech.pangchao.observerpattern;
+
+/**
+ * Created by pangchao on 2017/3/28.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Observer o1 = new ObserverImpl();
+        Observer o2 = new ObserverImpl();
+        Subject s1 = new SubjectImpl();
+
+        //注册观察者
+        s1.attach(o1);
+        s1.attach(o2);
+
+        //通知观察者
+        s1.notice();
+
+        //注销观察者
+        s1.detach(o1);
+    }
+}
---
+++ src/main/java/tech/pangchao/observerpattern/ObserverImpl.java
@@ -0,0 +1,12 @@
+package tech.pangchao.observerpattern;
+
+/**
+ * Created by pangchao on 2017/3/28.
+ */
+public class ObserverImpl implements Observer {
+
+    //响应方法
+    public void update() {
+        System.out.println("observer update...");
+    }
+}
