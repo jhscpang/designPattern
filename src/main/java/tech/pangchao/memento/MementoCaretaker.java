b683cb9b38ba899747cfc7e9ce1845e94c39abe3
pangchao03 <pangchao03@baidu.com>
2017-09-19 10:00:34 +0800

    update

---
+++ src/main/java/tech/pangchao/memento/MementoCaretaker.java
@@ -0,0 +1,18 @@
+package tech.pangchao.memento;
+
+/**
+ * Created by pangchao on 2017/4/6.
+ */
+
+//象棋棋子备忘录管理类:负责人
+public class MementoCaretaker {
+    private ChessmanMemento memento;
+
+    public ChessmanMemento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(ChessmanMemento memento) {
+        this.memento = memento;
+    }
+}
